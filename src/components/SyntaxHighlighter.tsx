095f35ce8a628726ad4e6322de02e9d9280159b1
Henry Li <yrnehli@proton.me>
2024-04-07 15:53:03 +0100

    fix eslint

--- src/components/SyntaxHighlighter.tsx
+++ src/components/SyntaxHighlighter.tsx
@@ -3,7 +3,6 @@ import { Light } from 'react-syntax-highlighter';
 import json from 'react-syntax-highlighter/dist/esm/languages/hljs/json';
 import { vs, vs2015 } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
 Light.registerLanguage('json', json);
 
 export type SyntaxHighlighterProps = {
