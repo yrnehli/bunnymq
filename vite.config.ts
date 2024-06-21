d9617c77f92408edec031b16773a19771b785a61
Henry Li <yrnehli@proton.me>
2024-06-21 20:45:03 +0100

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
-import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vite';
-import checker from 'vite-plugin-checker';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "/bunnymq/",
     plugins: [
         react(),
         checker({
