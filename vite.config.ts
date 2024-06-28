45ab618b056a313b560d6b5de48335b7fe6cc02e
Henry Li <44710606+yrnehli@users.noreply.github.com>
2024-06-28 10:14:09 +0100

    PWA Support (#9)
    * pwa manifest
    
    * rollup
    
    * vitepwa
    
    * icons
    
    * icons

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import checker from "vite-plugin-checker";
+import { VitePWA } from "vite-plugin-pwa";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
@@ -13,5 +14,24 @@ export default defineConfig({
         }),
         tsconfigPaths(),
         TanStackRouterVite(),
+        VitePWA({
+            manifest: {
+                name: "BunnyMQ",
+                icons: [
+                    {
+                        src: "/icon-192.png",
+                        type: "image/png",
+                        sizes: "192x192",
+                    },
+                    {
+                        src: "/icon-512.png",
+                        type: "image/png",
+                        sizes: "512x512",
+                    },
+                ],
+                start_url: "/",
+                display: "minimal-ui",
+            },
+        }),
     ],
 });
