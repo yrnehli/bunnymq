b963d6b67a05fbf1cda662a1e2f8d61efe413a56
Henry Li <yrnehli@proton.me>
2024-10-16 20:13:04 +0100

    pluralise util

--- src/routes/_authenticated/queues/$id/-components/QueueInfo.tsx
+++ src/routes/_authenticated/queues/$id/-components/QueueInfo.tsx
@@ -1,6 +1,6 @@
 import { Ping } from "@/components/Ping";
 import { Queue } from "@/lib/api";
-import { cn } from "@/lib/utils";
+import { cn, pluralise } from "@/lib/utils";
 
 export interface QueueInfoProps extends React.HTMLAttributes<HTMLDivElement> {
     queue: Queue;
@@ -16,21 +16,21 @@ export function QueueInfo({ queue, className, ...props }: QueueInfoProps) {
                 <Ping variant={queue.consumers > 0 ? "green" : "red"} />
                 <small>
                     <span className="font-bold">{queue.consumers}</span>{" "}
-                    {queue.consumers === 1 ? "consumer" : "consumers"}
+                    {pluralise(queue.consumers, { word: "consumer" })}
                 </small>
             </div>
             <div className="flex flex-row items-center gap-x-3">
                 <Ping variant={queue.ready > 0 ? "amber" : "green"} />
                 <small>
                     <span className="font-bold">{queue.ready}</span>{" "}
-                    {queue.ready === 1 ? "message" : "messages"} ready
+                    {pluralise(queue.ready, { word: "message" })} ready
                 </small>
             </div>
             <div className="flex flex-row items-center gap-x-3">
                 <Ping variant={queue.unacked > 0 ? "red" : "green"} />
                 <small>
                     <span className="font-bold">{queue.unacked}</span>{" "}
-                    {queue.unacked === 1 ? "message" : "messages"}{" "}
+                    {pluralise(queue.unacked, { word: "message" })}{" "}
                     unacknowledged
                 </small>
             </div>
